b4f5b87a5dd096a24115e5912ae109cc686457c1
Alena <alena308850@gmail.com>
2024-05-28 17:03:24 +0400

    start

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace ai_2
     {
         static void Main(string[] args)
         {
+            string[] questions = { "привет", "как тебя зовут", "как дела" };
+            string[] answers = { "здравствуйте", "Абоба", "хорошо" };
+
+            string question = null;
+
+            do
+            {
+                question = Console.ReadLine();
+
+                for (int i = 0; i < questions.Length; i++)
+                {
+                    if (question == questions[i])
+                    {
+                        Console.WriteLine(answers[i]);
+
+                        break;
+                    }
+                    else if (i == questions.Length - 1)
+                    {
+                        Console.WriteLine("Я еще маленький, объясни мне!");
+                    }
+                }
+            } while (true);
         }
     }
 }
